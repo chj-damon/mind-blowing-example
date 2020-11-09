bf404451c041269736ccba63f7aa98a8bfa3768a
chj_damon <chjdamon@gmail.com>
2020-11-09 18:53:25 +0800

    初始化环境

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet, StatusBar, Text, View } from 'react-native';
 
 export default function App() {
+  useEffect(() => {
+    StatusBar.setHidden(true);
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
